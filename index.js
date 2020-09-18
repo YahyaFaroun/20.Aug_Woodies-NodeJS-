463541f641e5a2e12ea2b33c739fdf0e54a5a6b9
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-19 00:08:59 +0200

    reload

---
+++ index.js
@@ -0,0 +1,80 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    console.log(req.method)
+    if (req.url === '/') {
+       fs.readFile('./public/index.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/about') {
+        fs.readFile('./public/about.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/how') {
+        fs.readFile('./public/howItWorks.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/categories') {
+        fs.readFile('./public/categories.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/testimony') {
+        fs.readFile('./public/testimony.html', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/style.css') {
+        fs.readFile('./public/style.css', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/css' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/Img/Logo.jpg') {
+        fs.readFile('./public/Img/Logo.jpg', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'image/jpg' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/Img/tisch.png') {
+        fs.readFile('./public/Img/tisch.png', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'image/png' })
+            res.end(data)
+        })
+    }
+    if (req.url === '/Img/route.png') {
+        fs.readFile('./public/Img/route.png', (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'image/png' })
+            res.end(data)
+        })
+    }
+})
+
+
+server.on('connection', () => {
+    console.log("connected")
+})
+
+server.listen(3000, () => {
+    console.log('server connected')
+})
